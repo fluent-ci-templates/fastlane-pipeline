1e548927463e42449a7b8ded36a9115c636920bd
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-03 18:11:39 +0000

    feat: allow running specific jobs from a pipeline

--- example/.fluentci/src/dagger/pipeline.ts
+++ example/.fluentci/src/dagger/pipeline.ts
@@ -1,8 +1,26 @@
-import Client, { connect } from "@dagger.io/dagger";
-import { buildRelease } from "./jobs.ts";
+import Client, {connect} from '@dagger.io/dagger';
+import * as jobs from './jobs.ts';
 
-export default function pipeline(src = ".") {
+const {buildRelease} = jobs;
+
+export default function pipeline(src = '.', args: string[] = []) {
   connect(async (client: Client) => {
+    if (args.length > 0) {
+      await runSpecificJobs(client, args);
+      return;
+    }
+
     await buildRelease(client, src);
   });
+}
+
+async function runSpecificJobs(client: Client, args: string[]) {
+  for (const name of args) {
+    // deno-lint-ignore no-explicit-any
+    const job = (jobs as any)[name];
+    if (!job) {
+      throw new Error(`Job ${name} not found`);
+    }
+    await job(client);
+  }
 }
