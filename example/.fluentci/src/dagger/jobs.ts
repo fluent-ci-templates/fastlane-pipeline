b19232b75d3b0e4e62d55ac3d800e375f5afdac5
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-25 05:41:07 +0000

    chore(example): run `fluentci init -t fastlane`

--- example/.fluentci/src/dagger/jobs.ts
+++ example/.fluentci/src/dagger/jobs.ts
@@ -1,33 +1,33 @@
-import Client from '@dagger.io/dagger';
-import {withDevbox} from 'https://deno.land/x/nix_installer_pipeline@v0.3.6/src/dagger/steps.ts';
-import {Dagger} from 'https://deno.land/x/android_pipeline@v0.2.3/mod.ts';
-import {withBaseAlpine, withEnv, withSrc} from './lib.ts';
+import Client from "@dagger.io/dagger";
+import { withDevbox } from "https://deno.land/x/nix_installer_pipeline@v0.3.6/src/dagger/steps.ts";
+import { Dagger } from "https://deno.land/x/android_pipeline@v0.2.3/mod.ts";
+import { withBaseAlpine, withEnv, withSrc } from "./lib.ts";
 
 export enum Job {
-  execLane = 'execLane',
+  execLane = "execLane",
 }
 
-const {withAndroidSdk} = Dagger;
+const { withAndroidSdk } = Dagger;
 
-export const execLane = async (client: Client, name: string, src = '.') => {
+export const execLane = async (client: Client, name: string, src = ".") => {
   const context = await client.host().directory(src);
   const baseCtr = withDevbox(
     withAndroidSdk(
       withBaseAlpine(
-        client.pipeline(Job.execLane).container().from('alpine:latest'),
-      ),
+        client.pipeline(Job.execLane).container().from("alpine:latest")
+      )
     )
-      .withMountedCache('/nix', client.cacheVolume('nix'))
-      .withMountedCache('/etc/nix', client.cacheVolume('nix-etc')),
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
   );
 
   const ctr = withEnv(withSrc(baseCtr, client, context))
-    .withEnvVariable('NODE_OPTIONS', '--max-old-space-size=4096')
-    .withExec(['sh', '-c', 'devbox run -- bun install'])
-    .withExec(['sh', '-c', 'devbox run -- bundle install'])
+    .withEnvVariable("NODE_OPTIONS", "--max-old-space-size=4096")
+    .withExec(["sh", "-c", "devbox run -- bun install"])
+    .withExec(["sh", "-c", "devbox run -- bundle install"])
     .withExec([
-      'sh',
-      '-c',
+      "sh",
+      "-c",
       `devbox run -- bundle exec fastlane android ${name}`,
     ]);
 
@@ -39,7 +39,7 @@ export const execLane = async (client: Client, name: string, src = '.') => {
 export type JobExec = (
   client: Client,
   name: string,
-  src?: string,
+  src?: string
 ) => Promise<void>;
 
 export const runnableJobs: Record<Job, JobExec> = {
@@ -47,5 +47,5 @@ export const runnableJobs: Record<Job, JobExec> = {
 };
 
 export const jobDescriptions: Record<Job, string> = {
-  [Job.execLane]: 'Executes a lane',
+  [Job.execLane]: "Executes a lane",
 };
