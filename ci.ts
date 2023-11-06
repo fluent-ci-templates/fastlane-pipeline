90e7a97112cebd68f30e87e1e942f638e026189f
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-11-06 19:17:36 +0300

    Update ci.ts

--- ci.ts
+++ ci.ts
@@ -1,3 +1,3 @@
-import { execLane } from "https://pkg.fluentci.io/fastlane_pipeline@v0.7.2/mod.ts";
+import { execLane } from "https://pkg.fluentci.io/fastlane_pipeline@v0.7.3/mod.ts";
 
 await execLane("buildRelease");
