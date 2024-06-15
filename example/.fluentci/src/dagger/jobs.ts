70a14d5e1309de8d30da3dbf715e958b3af4cd46
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-06-15 08:24:54 +0000

    use devbox run instead of devbox global run

--- example/.fluentci/src/dagger/jobs.ts
+++ example/.fluentci/src/dagger/jobs.ts
@@ -1,4 +1,4 @@
-import { Directory, Container, dag } from "../../deps.ts";
+import { type Directory, type Container, dag } from "../../deps.ts";
 import { withEnv, withSrc, getDirectory } from "./lib.ts";
 
 export enum Job {
@@ -22,20 +22,23 @@ export async function execLane(
   const baseCtr = dag
     .pipeline(Job.execLane)
     .container()
-    .from("ghcr.io/fluent-ci-templates/fastlane:latest");
+    .from("ghcr.io/fluent-ci-templates/fastlane:latest")
+    .withExec(["sh", "-c", "devbox update"]);
 
   const ctr = withEnv(withSrc(baseCtr, context))
     .withEnvVariable("NODE_OPTIONS", "--max-old-space-size=4096")
-    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && bun install'])
+    .withExec(["sh", "-c", "[ -f devbox.json ] || devbox init"])
     .withExec([
       "sh",
       "-c",
-      'eval "$(devbox global shellenv)" && bundle install',
+      "grep -q 'ruby' devbox.json || devbox add ruby@3.2.2 jdk@17.0.7+7",
     ])
+    .withExec(["sh", "-c", "devbox run -- bun install"])
+    .withExec(["sh", "-c", "devbox run -- bundle install"])
     .withExec([
       "sh",
       "-c",
-      `eval "$(devbox global shellenv)" && bundle exec fastlane android ${lane}`,
+      `devbox run -- bundle exec fastlane android ${lane}`,
     ]);
 
   await ctr.stdout();
