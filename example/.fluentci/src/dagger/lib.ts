01fc0923936726a228f9b57cf08474fa37a36d1e
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-06-17 17:26:49 +0000

    disable gradle cache

--- example/.fluentci/src/dagger/lib.ts
+++ example/.fluentci/src/dagger/lib.ts
@@ -36,9 +36,9 @@ export const getDirectory = async (
 
 export const withSrc = (ctr: Container, context: Directory) =>
   ctr
-    .withMountedCache("/app/android/.gradle", dag.cacheVolume("gradle"))
-    .withMountedCache("/app/android/app/build", dag.cacheVolume("build"))
-    .withMountedCache("/root/.gradle", dag.cacheVolume("gradle-cache"))
+    // .withMountedCache("/app/android/.gradle", dag.cacheVolume("gradle"))
+    // .withMountedCache("/app/android/app/build", dag.cacheVolume("build"))
+    // .withMountedCache("/root/.gradle", dag.cacheVolume("gradle-cache"))
     .withMountedCache("/app/vendor", dag.cacheVolume("vendor"))
     .withMountedCache("/app/node_modules", dag.cacheVolume("node_modules"))
     .withMountedCache(
