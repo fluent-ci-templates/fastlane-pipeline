cea44e7fa102e8d6a6496eafe2dd9bdd80f72ec6
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-06 08:05:15 +0000

    fix example

--- example/.fluentci/plugin/src/lib.rs
+++ example/.fluentci/plugin/src/lib.rs
@@ -16,3 +16,27 @@ pub fn exec_lane(args: String) -> FnResult<String> {
         .stdout()?;
     Ok(stdout)
 }
+
+#[plugin_fn]
+pub fn android(args: String) -> FnResult<String> {
+    setup_devbox()?;
+
+    let stdout = dag()
+        .devbox()?
+        .with_exec(vec!["devbox run -- bundle install"])?
+        .with_exec(vec!["devbox run -- bundle exec fastlane android", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn ios(args: String) -> FnResult<String> {
+    setup_devbox()?;
+
+    let stdout = dag()
+        .devbox()?
+        .with_exec(vec!["devbox run -- bundle install"])?
+        .with_exec(vec!["devbox run -- bundle exec fastlane ios", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
